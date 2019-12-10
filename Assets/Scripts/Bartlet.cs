4ff0ed6ed051755412305e79fe16baec8e9616f0
Dankiest Raft <7935418+Dancraft1@users.noreply.github.com>
2019-12-10 17:41:36 -0600

    added basic text boxes
    the reason this commit is so big is because of the sheer amount of examples and resources in TextMesh Pro. I need to erase those in a future commit

---
+++ Assets/Scripts/Bartlet.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bartlet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    int time=0;
+    void OnMouseDown()
+    {
+        if(time==0)
+            FindObjectOfType<TextBox>().DialogueWithTitle("Bartlet", "you are a fool and an idiot!");
+        else
+            FindObjectOfType<TextBox>().DialogueWithTitle("Bartlet", "why have thou pressed me a second time???");
+        time++;
+    }
+}
---
+++ Assets/Scripts/TextBox.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class TextBox : MonoBehaviour
+{
+    TextMeshProUGUI textMesh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textMesh = GetComponent<TextMeshProUGUI>();
+    }
+
+    public void EnableDialogueBox()
+    {
+        if(!GetComponent<TextMeshProUGUI>().isActiveAndEnabled)
+        {
+            GetComponent<TextMeshProUGUI>().enabled = true;
+            textMesh = GetComponent<TextMeshProUGUI>();
+        }
+        if(!GetComponentInParent<Image>().isActiveAndEnabled)
+            GetComponentInParent<Image>().enabled = true;
+    }
+
+   public void DisableDialogueBox()
+    {
+        if(GetComponent<TextMeshProUGUI>().isActiveAndEnabled)
+            GetComponent<TextMeshProUGUI>().enabled = false;
+        if(GetComponentInParent<Image>().isActiveAndEnabled)
+            GetComponentInParent<Image>().enabled = false;
+    }
+
+    public float speed = 10;
+    public bool isDialogueSkipped = false;
+    
+    public void DialogueWithTitle(string title, string text)
+    {
+        EnableDialogueBox();
+        StartCoroutine(DialogueWithTitleProcessor(title, text));
+    }
+    IEnumerator DialogueWithTitleProcessor(string title, string text)
+    {
+        isDialogueSkipped = false;
+        //todo: add a skip dialogue when another dialogue starts
+        string titleText = "<color=\"yellow\">" + title + ": ";
+        string bodyText = "<color=\"white\">";
+        foreach(char t in text)
+        {
+        bodyText += t;
+        textMesh.SetText(titleText + bodyText);
+        if(!isDialogueSkipped)
+            yield return new WaitForSecondsRealtime(1.0f/speed);
+        else
+            yield return new WaitForSecondsRealtime(0);
+        }
+    }
+    
+    public void Dialogue(string text)
+    {
+        EnableDialogueBox();
+        StartCoroutine(DialogueProcessor(text));
+    }
+    IEnumerator DialogueProcessor(string text)
+    {
+        isDialogueSkipped = false;
+        //todo: add a skip dialogue when another dialogue starts
+        string sentText = "";
+        foreach(char t in text)
+        {
+        sentText += t;
+        textMesh.SetText(sentText);
+        if(!isDialogueSkipped)
+            yield return new WaitForSecondsRealtime(1.0f/speed);
+        else
+            yield return new WaitForSecondsRealtime(0);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+            isDialogueSkipped = true;
+    }
+}
